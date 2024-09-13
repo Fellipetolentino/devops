86c7bc363de0f08a79e8db34e3748fb159fc29b6
Fellipetolentino <fellipe.tolentino@pucpr.edu.br>
2024-09-13 16:58:15 -0300

    Configuração inicial

--- main.py
+++ main.py
@@ -4,26 +4,5 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Para testar a calculadora use a porta /soma"}
+    return {"message": "Para testar a calculadora use a porta /soma, /subtracao, /multiplicacao e /divisao"}
 
-# Rota para a soma
-@app.get("/soma")
-def soma(a: float, b: float):
-    return {"resultado": a + b}
-
-# Rota para a subtração
-@app.get("/subtracao")
-def subtracao(a: float, b: float):
-    return {"resultado": a - b}
-
-# Rota para a multiplicação
-@app.get("/multiplicacao")
-def multiplicacao(a: float, b: float):
-    return {"resultado": a * b}
-
-# Rota para a divisão
-@app.get("/divisao")
-def divisao(a: float, b: float):
-    if b == 0:
-        raise HTTPException(status_code=400, detail="Divisão por zero não é permitida.")
-    return {"resultado": a / b}

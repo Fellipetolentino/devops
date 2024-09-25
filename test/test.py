ab252bd48f9f0f365d3ac44ab6abb01e7f0f7d0f
Fellipetolentino <fellipe.tolentino@pucpr.edu.br>
2024-09-25 19:03:26 -0300

    testes unitarios

---
+++ test/test.py
@@ -0,0 +1,17 @@
+from src.main import *
+
+
+def test_root():
+    assert root() == {"message": "Use as portas: /soma, /subtracao, /multiplicacao, /divisao ou /docs para usar interface interativa!"}
+
+def test_soma(a: float, b: float):
+    assert soma() == {"resultado": a + b}
+
+def test_subtracao(a: float, b: float):
+    assert subtracao() == {"resultado": a - b}
+
+def test_multiplicacao(a: float, b: float):
+    assert multiplicacao() == {"resultado": a * b}
+
+def test_divisao(a: float, b: float):
+    assert divisao() == {"resultado": a / b}

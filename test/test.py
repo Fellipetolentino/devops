7617bb9f467fdcdfae819eaa70d3bec12b4facf3
Fellipetolentino <fellipe.tolentino@pucpr.edu.br>
2024-09-25 19:30:20 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -6,14 +6,22 @@ def test_root():
     yield result
     assert result == {"message": "Use as portas: /soma, /subtracao, /multiplicacao, /divisao ou /docs para usar interface interativa!"}
 
-def test_soma(a: float, b: float):
-    assert soma() == {"resultado": a + b}
+def test_soma():
+    result = soma()
+    yield result
+    assert result == {"resultado": 10 + 5}
 
-def test_subtracao(a: float, b: float):
-    assert subtracao() == {"resultado": a - b}
+def test_subtracao():
+    result = subtracao()
+    yield result
+    assert result == {"resultado": 11 - 10}
 
-def test_multiplicacao(a: float, b: float):
-    assert multiplicacao() == {"resultado": a * b}
+def test_multiplicacao():
+    result = multiplicacao()
+    yield result
+    assert result == {"resultado": 5 * 5}
 
-def test_divisao(a: float, b: float):
-    assert divisao() == {"resultado": a / b}
+def test_divisao():
+    result = divisao()
+    yield result
+    assert result == {"resultado": 10 / 2}
